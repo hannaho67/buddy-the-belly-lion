d053c9e7c307738eae3b6781c8ec40363aaf23f5
hannaho67 <21OdeladeH@newsteadwood.co.uk>
2026-03-19 22:03:41 +0000

    number generated only when button A is pressed

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-let mode = 0
-let num = 0
-let tilt = 0
-
 input.onButtonPressed(Button.A, function () {
-        basic.showIcon(IconNames.Heart)
-        basic.pause(500)
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
-        let num = randint(1, 12)
-        basic.showNumber(num)
-        basic.pause(1000)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+    num2 = randint(1, 12)
+    basic.showNumber(num2)
+    basic.pause(1000)
 })
+let tilt = 0
+let num2 = 0
+let num = 0
 basic.forever(function () {
+    let mode = 0
     if (mode == 0) {
         basic.showIcon(IconNames.Happy)
     }
