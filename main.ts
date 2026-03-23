6b58450a4cb10d5ee819d986c04ac6df55699327
hannaho67 <21OdeladeH@newsteadwood.co.uk>
2026-03-23 15:31:01 +0000

    button A and button B and shake

--- main.ts
+++ main.ts
@@ -2,11 +2,25 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
     basic.pause(500)
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
-    num2 = randint(1, 12)
+    num2 = randint(1, 6)
+    basic.showNumber(num2)
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    num3 = randint(1, 12)
+    basic.showNumber(num3)
+    basic.pause(1000)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+    num2 = randint(1, 6)
     basic.showNumber(num2)
     basic.pause(1000)
 })
 let tilt = 0
+let num3 = 0
 let num2 = 0
 let num = 0
 basic.forever(function () {
