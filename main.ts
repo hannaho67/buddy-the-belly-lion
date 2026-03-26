c3023ec2aa9443662d50fd406ecf349cb17a457e
hannaho67 <21OdeladeH@newsteadwood.co.uk>
2026-03-26 17:39:31 +0000

    budy final ipad

--- main.ts
+++ main.ts
@@ -4,12 +4,34 @@ input.onButtonPressed(Button.A, function () {
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
     num2 = randint(1, 6)
     basic.showNumber(num2)
-    basic.pause(1000)
+    basic.pause(500)
 })
 input.onButtonPressed(Button.B, function () {
-    num3 = randint(1, 12)
+    num3 = randint(6, 12)
     basic.showNumber(num3)
-    basic.pause(1000)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+    basic.pause(500)
+    if (num3 == 6) {
+        basic.showString("DINNER")
+    }
+    if (num3 == 7) {
+        basic.showString("PJ")
+    }
+    if (num3 == 8) {
+        basic.showString("BRUSH")
+    }
+    if (num3 == 9) {
+        basic.showString("STORY")
+    }
+    if (num3 == 10) {
+        basic.showString("BEDTIME")
+    }
+    if (num3 == 11) {
+        basic.showString("SLEEP")
+    }
+    if (num3 == 12) {
+        basic.showString("DREAM")
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Heart)
@@ -30,10 +52,5 @@ basic.forever(function () {
     }
     if (mode == 2) {
         tilt = input.rotation(Rotation.Roll)
-        if (tilt > 30) {
-            basic.showArrow(ArrowNames.East)
-        } else if (tilt < -30) {
-            basic.showArrow(ArrowNames.West)
-        }
     }
 })
